d211be377421c399c847d8cb5054bc82e66ac097
Stephen <stephen.catacte@gmail.com>
2023-08-16 09:33:50 +0800

    add dynamic socials

--- components/includes/Footer.jsx
+++ components/includes/Footer.jsx
@@ -1,31 +1,45 @@
+import Image from "next/image";
+import { FaFacebookSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
 
-import Image from "next/image"
-import { FaFacebookSquare,FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+import { getData } from "@/lib/data";
 
-const Footer = () => {
+const Footer = async () => {
+  const c = await getData();
+
   return (
     <div className="container px-8 mx-auto xl:px-5  max-w-screen-lg py-5 lg:py-8 mt-10 border-t border-gray-100 dark:border-gray-800">
       <div className="text-center text-sm">
-        Copyright &copy; 2023 <span className="">iContent Framework</span>. All rights reserved.
+        Copyright &copy; 2023 <span className="">iContent Framework</span>. All
+        rights reserved.
       </div>
       <div className="mt-1 flex justify-center gap-1 text-center text-sm text-gray-500 dark:text-gray-600">
         <span>
-          Made by <a href="#" rel="noopener" target="_blank">Contrib.com</a>
+          Made by{" "}
+          <a href="#" rel="noopener" target="_blank">
+            Contrib.com
+          </a>
         </span>
         <span>·</span>
         <span>
-          <a href="#" rel="noopener" target="_blank">Vnoc.com</a>
+          <a href="#" rel="noopener" target="_blank">
+            Vnoc.com
+          </a>
         </span>
       </div>
       <div className="mt-2 flex items-center justify-between">
         <div className="mt-5">
-          <a href="#" target="_blank" rel="noopener" className="relative block w-44">
-            <Image 
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener"
+            className="relative block w-44"
+          >
+            <Image
               src="https://cdn.vnoc.com/logos/logo-new-contrib-06.png"
-              alt="Powered by Vercel" 
-              loading="lazy" 
-              width={0} 
-              height={0} 
+              alt="Powered by Vercel"
+              loading="lazy"
+              width={0}
+              height={0}
               sizes="125px"
               className="max-w-full h-auto w-full"
             />
@@ -37,18 +51,27 @@ const Footer = () => {
         <div className="inline-flex items-center flex-col pt-8">
           <ul className="mb-0 space-x-1">
             <li className="inline-block">
-              <a href="#" className="inline-block no-underline text-[#3b5998]">
-                <FaFacebookSquare className="w-6 h-6"/>
+              <a
+                href={c.data.fb}
+                className="inline-block no-underline text-[#3b5998]"
+              >
+                <FaFacebookSquare className="w-6 h-6" />
               </a>
             </li>
             <li className="inline-block">
-              <a href="#" className="inline-block no-underline text-[#0A66C2]">
-                <FaLinkedin className="w-6 h-6"/>
+              <a
+                href={c.data.linkedin}
+                className="inline-block no-underline text-[#0A66C2]"
+              >
+                <FaLinkedin className="w-6 h-6" />
               </a>
             </li>
             <li className="inline-block">
-              <a href="#" className="inline-block no-underline text-[#00acee]">
-                <FaTwitterSquare className="w-6 h-6"/>
+              <a
+                href={c.data.twitter}
+                className="inline-block no-underline text-[#00acee]"
+              >
+                <FaTwitterSquare className="w-6 h-6" />
               </a>
             </li>
           </ul>
@@ -56,7 +79,7 @@ const Footer = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
