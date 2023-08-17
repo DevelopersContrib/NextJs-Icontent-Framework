8098ba5516eeca89ee7ce9dc034ba2df45a5c6e1
Stephen <stephen.catacte@gmail.com>
2023-08-17 14:42:05 +0800

    hide view all post

--- app/page.jsx
+++ app/page.jsx
@@ -20,14 +20,14 @@ const Home = async () => {
         <div className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3 ">
           <BlogPosts blogs={blogs} />
         </div>
-        <div className="mt-10 flex justify-center">
+        {/* <div className="mt-10 flex justify-center">
           <a
             className="relative inline-flex items-center gap-1 rounded-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300"
             href="#"
           >
             <span>View all Posts</span>
           </a>
-        </div>
+        </div> */}
       </div>
       <Footer />
     </>
