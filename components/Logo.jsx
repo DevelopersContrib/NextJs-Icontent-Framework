93789569c2425e0c3001366f7839da6e2c62cb9d
Stephen <stephen.catacte@gmail.com>
2023-08-18 14:04:03 +0800

    Domain name Capitalize

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -5,6 +5,6 @@ export default function Logo({ domain, logo }) {
   if (logo != null && logo != '') {
     return <Image src={logo} width={120} height={120} alt="" className="d-inline-flex img-fluid mb-3 tw-pr-3" />;
   } else {
-    return <Navbar.Brand href="#home">{domain}</Navbar.Brand>;
+    return <Navbar.Brand href="#home">{domain.charAt(0).toUpperCase() + domain.substring(1)}</Navbar.Brand>;
   }
 }
