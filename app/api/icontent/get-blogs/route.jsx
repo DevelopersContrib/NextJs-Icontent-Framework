481160a7f51debbc3119e78510cfc3f88790f3b4
ron <row.none@gmail.com>
2023-08-23 21:57:19 -0700

    fixed api route

--- app/api/icontent/get-blogs/route.jsx
+++ app/api/icontent/get-blogs/route.jsx
@@ -1,19 +1,15 @@
 import axios from 'axios';
 
-export const GET = async (request) => {
+export const GET = async (request, { params }) => {
   try {
-    const urlParams = new URL(request.url);
-    const page = urlParams.searchParams.get('page');
-    const limit = urlParams.searchParams.get('limit');
-    const search = urlParams.searchParams.get('search');
-
-    const url = process.env.CONTRIB_API1 + 'icontent/getblogs?key=' + process.env.CONTRIB_API_KEY + '&domain=' + process.env.NEXT_PUBLIC_VERCEL_URL + '&page=' + page + '&limit=' + limit + '&search=' + search;
-
-    const res = await axios.get(url);
-    const result = res.data;
-
-    return new Response(JSON.stringify(result.data), { status: 201 });
+      const limit = params.limit;
+      const page = params.page;
+      const search = params.search;
+      const url = process.env.CONTRIB_API1 + 'icontent/getblogs?key=' + process.env.CONTRIB_API_KEY + '&domain=' + process.env.NEXT_PUBLIC_VERCEL_URL + '&page=' + page + '&limit=' + limit + '&search=' + search;
+      const res = await axios.get(url);
+      const result = res.data;
+      return new Response(JSON.stringify(result.data), { status: 200 })
   } catch (error) {
-    console.log('Error: ', error);
+      return new Response("Failed to fetch prompts created by user", { status: 500 })
   }
-};
+} 
