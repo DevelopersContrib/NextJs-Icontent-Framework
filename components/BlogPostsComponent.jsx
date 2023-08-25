e44ecb79320402acfd8b12c9782f41ac5cda97c4
Kareen <kjabellar@gmail.com>
2023-08-25 11:45:35 +0800

    updated blog link

--- components/BlogPostsComponent.jsx
+++ components/BlogPostsComponent.jsx
@@ -71,19 +71,19 @@ const BlogPosts = () => {
               blogs.map((blog, index) => (
                 <div className="group cursor-pointer" key={index}>
                   <div className=" overflow-hidden rounded-md bg-gray-100 transition-all hover:scale-105 dark:bg-gray-800">
-                    <a href={`/blog/${createSlug(blog.blog_title)}`} className="relative block aspect-video">
+                    <a href={`/blog/${blog.slug}`} className="relative block aspect-video">
                       <Image priority width={0} height={0} sizes="100vw" className="object-cover transition-all max-w-full h-[180px] w-full" alt={blog.blog_title} src={blog.blog_image ? blog.blog_image : '/default-no-image.png'} />
                     </a>
                   </div>
                   <div>
                     <div>
                       <div className="flex gap-3">
-                        <a href={`/blog/${createSlug(blog.blog_title)}`}>
+                        <a href={`/blog/${blog.slug}`}>
                           <span className="inline-block text-xs font-medium tracking-wider uppercase   mt-5 text-blue-600">{blog.blog_title}</span>
                         </a>
                       </div>
                       <h2 className="text-lg font-semibold leading-snug tracking-tight mt-2 dark:text-white">
-                        <a href={`/blog/${createSlug(blog.blog_title)}`}>
+                      <a href={`/blog/${blog.slug}`}>
                           <span className="bg-gradient-to-r from-green-200 to-green-100 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px] dark:from-purple-800 dark:to-purple-900">
                             {blog.blog_content.substring(0, 100)}
                           </span>
@@ -91,7 +91,7 @@ const BlogPosts = () => {
                       </h2>
                       <div className="hidden">
                         <p className="mt-2 line-clamp-3 text-sm text-gray-500 dark:text-gray-400">
-                          <a href={`/blog/${createSlug(blog.blog_title)}`}>
+                        <a href={`/blog/${blog.slug}`}>
                             Reinvention often comes in spurts, after a long period of silence. Just as modern architecture recently enjoyed a comeback, brand architecture, a field with well-established principles for decades
                           </a>
                         </p>
